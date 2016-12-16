d0aa3bf353c3edff0e8d41028d22e4de9bc90e01
vikok <vikskopkop@gmail.com>
2016-12-16 17:39:44 +0200

    add ul

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 ;(function(){
 
 	"use strict";
-	$( "#tabs" ).tabs();
-
-	tabs.on('click', 'div', function(){
-		var clicked = $(this);
-		show('.ba-tabs');
-
-
+	$(document).ready(function() {
+  	$('#tabs').click(function(e) {
+  	var currentValue = $(this);
+//активируем необходимую вкладку и скрываем остальные
+  	$('#tabs ' + currentValue).fadeIn(500).siblings().hide();
+// добавляем и удаляем класс active для необходимого пункта меню
+  	$(this).parent('li').addClass('active').siblings().removeClass('active');
+  	e.preventDefault();
+  	});
 	});
 
 })(jQuery);
